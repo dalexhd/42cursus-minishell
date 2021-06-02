053746f1455dc2ae7f2dad7c9b6bb56aaa088026
Enrique jose Vila rodriguez <evila-ro@c1r2s1.42madrid.com>
2021-06-02 18:40:49 +0200

    quotes norma

--- srcs/parser.c
+++ srcs/parser.c
@@ -1,49 +1,48 @@
 #include "../includes/minishell.h"
 
-t_bool	valid_quotes(char *cmd)
+static int	quotes(char *cmd, int i, char c)
 {
-	int	i;
-	int	one;
-	int	two;
-	int	pos;
-
-	i = 0;
-	one = 0;
-	two = 0;
-	while (cmd[i])
-	{
-		if (cmd[i] == '"' && cmd[i])
-		{
-			two++;
-			pos = i;
-			i++;
-			while (cmd[i] && cmd[i] != '"')
-				i++;
-			if (!cmd[i])
-				break ;
-			else if (cmd[i] == '"')
-				two--;
-		}
-		if (cmd[i] == '\'' && cmd[i])
-		{
-			one++;
-			pos = i;
-			i++;
-			while (cmd[i] && cmd[i] != '\'')
-				i++;
-			if (!cmd[i])
-				break ;
-			else if (cmd[i] == '\'')
-				one--;
-		}
+	while (cmd[i] && cmd[i] != c)
 		i++;
-	}
-	if (two || one)
+	if (!cmd[i])
+		return (-42);
+	return (i);
+}
+
+t_bool	tof_quotes(char *cmd, int pos, char one)
+{
+	if (one)
 	{
-		ft_error("minishell: Unmaching opening quotes at col %d: %s\n", 0, pos, cmd + (pos));
+		ft_error("minishell: Unmaching opening quotes at col %d: %s\n",
+			0, pos, cmd + (pos));
 		return (false);
 	}
 	return (true);
+}
+
+t_bool	valid_quotes(char *cmd)
+{
+	int		i;
+	char	one;
+	int		pos;
+
+	i = 0;
+	one = 0;
+	while (cmd[i])
+	{
+		if ((cmd[i] == '\'' || cmd[i] == '"') && cmd[i])
+		{
+			one = cmd[i];
+			pos = i++;
+			i = quotes(cmd, i, one);
+			if (i == -42)
+				break ;
+			else if (cmd[i] == one)
+				one = 0;
+		}
+		i++;
+	}
+	return (tof_quotes(cmd, pos, one));
 }
 
 t_list	*parse_args(char *cmd)
