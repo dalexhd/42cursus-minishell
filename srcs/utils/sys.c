8e678665e26af66f5c937ad7c2f07c6c54163ec3
aborboll <aborboll@student.42.fr>
2021-07-05 22:02:13 +0200

    feat: fix bit sys.c

--- srcs/utils/sys.c
+++ srcs/utils/sys.c
@@ -1,66 +1,6 @@
 #include "../../includes/minishell.h"
 
-void	exec(t_shell *shell, t_parsed *parsed)
-{
-	char	*arg;
-	char	**args;
-	char	*pwd;
-	t_bool	force_exit;
-
-	force_exit = false;
-	if (!parsed->args)
-		return ;
-	arg = (char *)parsed->args->content->cmd;
-	args = ft_safesplit(shell, parsed->args);
-	shell->status = 0;
-	if (parsed->args->content->is_builtin)
-	{
-		if (!ft_strcmp(arg, "cd"))
-			ft_cd(shell, args);
-		else if (!ft_strcmp(arg, "exit"))
-			ft_exit(args);
-		else if (!ft_strcmp(arg, "export"))
-			ft_export(shell, args);
-		else if (!ft_strcmp(arg, "unset"))
-			ft_unset(shell, args);
-		else if (!ft_strcmp(arg, "env"))
-			ft_env(shell);
-		else if (!ft_strcmp(arg, "pwd"))
-		{
-			pwd = ft_pwd();
-			ft_printf("%s\n", pwd);
-			ft_strdel(&pwd);
-		}
-		else if (!ft_strcmp(arg, "echo"))
-			ft_echo(args);
-	}
-	else if (parsed->args->content->bin_path && execve(parsed->args->content->bin_path, args, shell->envp) == -1)
-	{
-		if (is_directory(parsed->args->content->bin_path))
-			ft_error("minishell: %s: is a directory\n", 126, args[0]);
-		else if (!has_access(parsed->args->content->bin_path))
-			ft_error("minishell: %s: Permission denied\n", 126, args[0]);
-		else if (ft_strncmp(parsed->args->content->bin_path, "./", 2) == 0)
-			ft_error("minishell: %s: No such file or directory\n", 127, args[0]);
-		else
-			ft_error("minishell: %s: command not found\n", 127, args[0]);
-	}
-	ft_split_del(args);
-	if (force_exit)
-	{
-		clear_cmd(shell);
-		exit(0);
-	}
-}
-static int	renegado(int **pipes, int j)
-{
-	close(pipes[j][1]);
-	close(pipes[j][0]);
-	j++;
-	return (j);
-}
-
-static void	handle_redirect(t_slist	*list)
+/* static void	handle_redirect(t_slist	*list)
 {
 	int		input;
 	t_rlist	*redirect;
@@ -96,177 +36,125 @@ static void	handle_redirect(t_slist	*list)
 		}
 		redirect = redirect->next;
 	}
+} */
+
+static	void	dup_close(int old, int new)
+{
+	if (dup2(old, new) < 0)
+		ft_error("Could not dup!", STDOUT_FILENO);
+	if (close(old) < 0)
+		ft_error("Could not close!", STDOUT_FILENO);
 }
 
-void	run(t_shell *shell)
+/**
+ * Here we exec the builtin
+ *
+**/
+void	ft_exec_builtin(t_shell *shell, t_slist *parsed)
+{
+	(void)shell;
+	(void)parsed;
+	return ;
+}
+
+/**
+ * Here we exec the system builtin
+ *
+**/
+void	ft_exec_bin(t_shell *shell, t_slist *parsed)
+{
+	t_alist	*args;
+	char	**args_split;
+
+	args = parsed->content->args;
+	args_split = ft_safesplit(shell, args);
+	if (!args_split[0])
+		exit(0);
+	if (args->content->bin_path
+		&& execve(args->content->bin_path, args_split, shell->envp) == -1)
+	{
+		if (is_directory(args->content->bin_path))
+			ft_error("minishell: %s: is a directory\n", 126, args[0]);
+		else if (!has_access(args->content->bin_path))
+			ft_error("minishell: %s: Permission denied\n", 126, args[0]);
+		else if (ft_strncmp(args->content->bin_path, "./", 2) == 0)
+			ft_error("minishell: %s: No such file or directory\n", 127, args[0]);
+		else
+			ft_error("minishell: %s: command not found\n", 127, args[0]);
+	}
+}
+
+void	ft_exec_child(t_shell *shell, t_slist *parsed, int *fd, int prepipe)
+{
+	if (prepipe)
+		dup_close(prepipe, 0);
+	if (parsed->content->flags.has_stdin != 0)
+		dup_close(STDIN_FILENO, 0);
+	if (parsed->next)
+		dup_close(fd[1], 1);
+	if (parsed->content->flags.has_stdout != 1)
+		dup_close(STDOUT_FILENO, 0);
+	if (parsed->next)
+		close(fd[0]);
+	if (parsed->content->args->content->is_builtin)
+		ft_exec_builtin(shell, parsed);
+	ft_exec_bin(shell, parsed);
+}
+
+void	ft_exec_cmd(t_shell *shell, t_slist *parsed, int prepipe)
+
 {
 	pid_t	pid;
-	pid_t	*pids;
-	t_slist	*list;
-	int		**pipes;
-	t_bool	*whitelist;
-	int		input;
-	int		i;
-	int		j;
-	int		k;
+	int		fd[2];
 	int		status;
 
-	list = shell->parsed;
-	if (!list)
+	status = 0;
+	if (parsed->next && pipe(fd) < 0)
+	{
+		ft_error("bad pipe openning", false);
 		return ;
-	pids = malloc(ft_slstsize(list) * sizeof(int) + 1);
-	whitelist = malloc(ft_slstsize(list) * sizeof(t_bool) + 1);
-	pipes = (int **) malloc ((ft_slstsize(list) - 1) * sizeof(int *));
-	i = 0;
-	while (i < ft_slstsize(list) - 1)
-	{
-		pipes[i] = (int *) malloc (2 * sizeof(int));
-		if (pipe(pipes[i]) < 0)
-			ft_error("Fallo all crear el pipe %s\n", 1, strerror(errno));
-		i++;
 	}
-	i = 0;
-	while (list)
+	pid = fork();
+	if (pid < 0)
 	{
-		while (list->content->args)
-		{
-			if (list->content->args->content)
-				break;
-			list->content->args = list->content->args->next;
-		}
-		if (!list->content->args)
-			break;
-		whitelist[i] = true;
-		if (list->content->args->content->is_builtin)
-		{
-			exec(shell, list->content);
-			whitelist[i] = false;
-		}
-		else if (ft_slstsize(shell->parsed) == 1)
-		{
-			pid = fork();
-			signal_handler();
-			if (pid < 0)
-				ft_error("Fallo el fork() %s\n", 1, strerror(errno));
-			else if (pid == 0)
-			{
-				handle_redirect(list);
-				exec(shell, list->content);
-			}
-			else
-			{
-				waitpid(pid, &status, 0);
-				if (WIFSIGNALED(status))
-				{
-					shell->status = WTERMSIG(status);
-					if (shell->status == 2)
-						shell->status = 130;
-				}
-				else if (WIFEXITED(status) != 0 && WEXITSTATUS(status) != 0)
-					shell->status = WEXITSTATUS(status);
-			}
-		}
-		else
-		{
-			pid = fork();
-			if (pid < 0)
-				ft_error("Fallo el fork() %s\n", 1, strerror(errno));
-			else if (pid == 0)
-			{
-				if (i == 0)
-				{
-					if (list->content->flags.redirect.in.status)
-					{
-						input = open(list->content->flags.redirect.in.file,
-								O_RDONLY, 0600);
-						if (input < 0)
-							return (ft_error("minishell: %s: %s\n", true,
-									list->content->flags.redirect.in.file,
-									strerror(errno)));
-						dup2(input, STDIN_FILENO);
-					}
-					j = 1;
-					while (j < ft_slstsize(shell->parsed) - 1)
-						j = renegado(pipes, j);
-					close(pipes[0][0]);
-					dup2(pipes[0][1], 1);
-				}
-				if (i > 0 && i < ft_slstsize(shell->parsed) - 1)
-				{
-					if (i == 1 && ft_slstsize(shell->parsed) != 3)
-					{
-						j = i + 1;
-						while (j < ft_slstsize(shell->parsed) - 1)
-							j = renegado(pipes, j);
-					}
-					if (i == ft_slstsize(shell->parsed) - 2
-						&& ft_slstsize(shell->parsed) != 3)
-					{
-						j = 0;
-						while (j < i - 1)
-							j = renegado(pipes, j);
-					}
-					if (i != 1 && i != ft_slstsize(shell->parsed) - 2
-						&& ft_slstsize(shell->parsed) != 3)
-					{
-						j = 0;
-						while (j < i - 1)
-							j = renegado(pipes, j);
-						j = i + 1;
-						while (j < ft_slstsize(shell->parsed) - 1)
-							j = renegado(pipes, j);
-					}
-					close(pipes[i - 1][1]);
-					dup2(pipes[i - 1][0], 0);
-					close(pipes[i][0]);
-					dup2(pipes[i][1], 1);
-				}
-				if (i == ft_slstsize(shell->parsed) - 1)
-				{
-					handle_redirect(list);
-					j = 0;
-					while (j < ft_slstsize(shell->parsed) - 2)
-						j = renegado(pipes, j);
-					close(pipes[i - 1][1]);
-					dup2(pipes[i - 1][0], 0);
-				}
-				exec(shell, list->content);
-			}
-			else
-				pids[i] = pid;
-		}
-		list = list->next;
-		i++;
+		ft_error("Bad fork!", false);
+		return ;
 	}
-	k = 0;
-	while (k < ft_slstsize(shell->parsed) - 1)
-		k = renegado(pipes, k);
-	k = 0;
-	while (k < ft_slstsize(shell->parsed))
+	if (pid == 0)
+		ft_exec_child(shell, parsed, fd, prepipe);
+	if (prepipe && close(prepipe) < 0)
 	{
-		if (whitelist[k])
-		{
-			waitpid(pids[k], &status, 0);
-			if (WIFSIGNALED(status))
-			{
-				shell->status = WTERMSIG(status);
-				if (shell->status == 2)
-					shell->status = 130;
-			}
-			else if (WIFEXITED(status) != 0 && WEXITSTATUS(status) != 0)
-				shell->status = WEXITSTATUS(status);
-			else if (WEXITSTATUS(status) == 0)
-				shell->status = 0;
-		}
-		k++;
+		ft_error("Bad close!", false);
+		return ;
 	}
-	i = 0;
-	while (i < ft_slstsize(shell->parsed) - 1)
+	if (parsed->next && close(fd[1]) < 0)
 	{
-		free(pipes[i]);
-		i++;
+		ft_error("Bad pipe!", false);
+		return ;
 	}
-	free(pipes);
-	free(whitelist);
-	free(pids);
+	if (parsed->next)
+		ft_exec_cmd(shell, parsed->next, fd[0]);
+	if (waitpid(pid, &status, 0) < 0)
+	{
+		ft_error("Bad pipe!", false);
+		return ;
+	}
+	if (parsed->next)
+		shell->status = WEXITSTATUS(status);
+	if (!parsed->next && WIFSIGNALED(status))
+		shell->status = WTERMSIG(status) + 128;
+}
+
+/*
+**	- If we only have 1 command, and it's an minishell builtin
+**	we need to execute it without forks.
+**	- If we get more thatn 1 arguments, then we call `ft_exec_cmd`
+**	recursive function.
+*/
+void	run(t_shell *shell)
+{
+	if (shell->parsed)
+	{
+		ft_exec_cmd(shell, shell->parsed, 0);
+	}
 }
