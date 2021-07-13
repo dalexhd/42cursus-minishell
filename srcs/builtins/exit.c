73fa8e0ba3c9255826b3a771fe33b830b7fbdbd2
DalexHD <eldalexhd@gmail.com>
2021-07-13 16:52:36 +0000

    feat: fix exit norma.

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -7,8 +7,6 @@ void	ft_exit(t_shell *shell, char **args)
 	t_llong	number;
 
 	i = 1;
-	if (!args[1])
-		g_running = false;
 	arg = ft_strtrim(args[1], " ");
 	number = ft_atoll(arg);
 	if ((((arg[0] == '-' && (!arg[1] || !ft_strevery(&arg[1], ft_isdigit)))
@@ -20,18 +18,13 @@ void	ft_exit(t_shell *shell, char **args)
 	{
 		ft_error("minishell: exit: %s: numeric argument required\n",
 			0, args[i]);
-		g_running = false;
 		shell->status = 2;
 	}
 	else if (args[i + 1])
 	{
 		ft_error("minishell: exit: too many arguments\n", 0);
-		g_running = false;
 		shell->status = 1;
 	}
-	else
-	{
-		g_running = false;
-		shell->status = number;
-	}
+	g_running = false;
+	shell->status = number;
 }
