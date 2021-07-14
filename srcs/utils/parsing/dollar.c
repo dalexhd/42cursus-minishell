7529c9adfe3b8dab8e8c9ba3bef7db4312d07bdf
DalexHD <eldalexhd@gmail.com>
2021-07-14 17:43:21 +0000

    fix: dollar sign.

--- srcs/utils/parsing/dollar.c
+++ srcs/utils/parsing/dollar.c
@@ -16,8 +16,6 @@ static	void	parse_aux(char *aux, char **line, char *tmp)
 		*line = ft_strjoin_free(*line, ft_strdup(aux));
 	else if (ft_isdigit(tmp[0]))
 		*line = ft_strjoin_free(*line, ft_strcut(tmp, 2, ft_strlen(tmp)));
-	else
-		*line[ft_strlen(*line)] = DEL;
 }
 
 void	parse_dollar(t_shell *shell, char **cmd, int *i, char **line)
