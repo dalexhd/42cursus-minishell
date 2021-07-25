76cba18b9148b00d7c71a77d006a7fb885852855
DalexHD <eldalexhd@gmail.com>
2021-07-25 10:58:57 +0000

    feat: fix a bit the norma of clean_str.

--- srcs/utils/clear_strings.c
+++ srcs/utils/clear_strings.c
@@ -1,17 +1,35 @@
 #include "../../includes/minishell.h"
 
-static void	no_quot(t_shell *shell, char *c, int *i, char *res)
+static	t_bool	clean_str_sec(t_args *arg, int *i, char **res, char **c)
 {
-	if (c[*i] == '\\')
+	t_bool		ret;
+
+	ret = false;
+	if (arg->quot_type == N_QUOT && (*c)[*i] == '\\')
 	{
-		ft_strncat(res, &c[*i + (ft_strchr("`\\", c[*i + 1]) == NULL)], 1);
+		ft_strncat(*res,
+			&(*c)[*i + (ft_strchr("`\\", (*c)[*i + 1]) == NULL)], 1);
 		(*i)++;
+		ret = true;
 	}
-	else if (c[*i] == '~')
-		parse_tilde(shell, c, i, &(res));
+	else if (arg->quot_type == S_QUOT
+		&& (*c)[*i] == '\\' && (*c)[*i + 1] == '\'')
+	{
+		(*c)[*i + 1] = DEL;
+		ret = true;
+	}
+	else if ((arg->quot_type != D_QUOT && (*c)[*i] == '\\'
+		&& (*c)[*i + 1] == '\\')
+		|| (arg->quot_type == D_QUOT && (*c)[*i] == '\\'
+			&& ft_strchr("\"\\$`\\", (*c)[*i + 1])))
+	{
+		(*c)[*i] = DEL;
+		ret = true;
+	}
+	return (ret);
 }
 
-char	*clean_str(t_shell *shell, t_args *a, char *c)
+char	*clean_str(t_shell *shell, t_args *arg, char *c)
 {
 	int		i;
 	char	*res;
@@ -19,39 +37,28 @@ char	*clean_str(t_shell *shell, t_args *a, char *c)
 	char	*tmp;
 
 	res = ft_calloc(ft_strlen(c) + 1, sizeof(char *));
-	status = NULL;
 	i = 0;
 	while (i < (int)ft_strlen(c))
 	{
-		if (a->quot_type == N_QUOT)// && c[i] == '\\')
-			no_quot(shell, c, &i, res);
-	//	{
-	//		ft_strncat(res, &c[i + (ft_strchr("`\\", c[i + 1]) == NULL)], 1);
-	//		i++;
-	//	}
-		else if (a->quot_type == S_QUOT && c[i] == '\\' && c[i + 1] == '\'')
-			c[i + 1] = DEL;
-		else if ((a->quot_type != D_QUOT && c[i] == '\\' && c[i + 1] == '\\')
-			|| (a->quot_type == D_QUOT && c[i] == '\\'
-				&& ft_strchr("\"\\$`\\", c[i + 1])))
-			c[i] = DEL;
-		else if (a->quot_type != S_QUOT && c[i] == '$'
-			&& (ft_isalnum(c[i + 1]) || c[i + 1] == '_'))
-			parse_dollar(shell, &c, &i, &res);
-//		else if (a->quot_type == N_QUOT && c[i] == '~')
-//			parse_tilde(shell, c, &i, &res);
-		else if (!ft_strncmp(&c[i], "$?", 2))
+		if (!clean_str_sec(arg, &i, &res, &c))
 		{
-			status = ft_itoa(shell->status);
-			ft_strncat(res, status, ft_strlen(status));
-			i++;
+			if (arg->quot_type != S_QUOT && c[i] == '$'
+				&& (ft_isalnum(c[i + 1]) || c[i + 1] == '_'))
+				parse_dollar(shell, &c, &i, &res);
+			else if (arg->quot_type == N_QUOT && c[i] == '~')
+				parse_tilde(shell, c, &i, &res);
+			else if (!ft_strncmp(&c[i], "$?", 2))
+			{
+				status = ft_itoa(shell->status);
+				ft_strncat(res, status, ft_strlen(status));
+				ft_strdel(&status);
+				i++;
+			}
+			else
+				ft_strncat(res, &c[i], 1);
 		}
-		else
-			ft_strncat(res, &c[i], 1);
 		i++;
 	}
-	if (status)
-		ft_strdel(&status);
 	tmp = ft_strdup(res);
 	ft_strdel(&res);
 	return (tmp);
